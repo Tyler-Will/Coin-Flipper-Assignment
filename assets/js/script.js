14a8d3b15a913758a2285f9c61e80c859d8727bb
Tyler-Will <tylerceeme@gmail.com>
2022-08-04 22:55:47 -0700

    First commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,47 @@
-// TODO: Declare any global variables we need
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
 
+// TODO: Declare any global variables we need
+let head_tot = 0;
+let tail_tot = 0;
+let head_percent= head_tot/tail_tot * 100;
+let tail_percent = tail_tot/head_tot * 100;
+
+document.getElementById("Coinflip").onclick = function(){
+   
+    x = Math.floor(Math.random() *2);
+    if (x < 1){
+        document.getElementById("heads").innerHTML= head_tot
+        head_tot = head_tot+1
+        document.getElementById("heads-percent").innerHTML = head_tot/tail_tot * 100
+        document.getElementById("tails-percent").innerHTML = tail_tot/head_tot * 100
+        
+    }
+    else{
+        document.getElementById("tails").innerHTML= tail_tot
+       tail_tot = tail_tot+1
+       document.getElementById("heads-percent").innerHTML = head_tot/tail_tot * 100
+      document.getElementById("tails-percent").innerHTML = tail_tot/head_tot * 100
+    
+    
+}
+}
+document.getElementById("Clearscore").onclick = function(){
+    //if (head_tot > 0)
+    document.getElementById("heads").innerHTML= 0
+    document.getElementById("heads-percent").innerHTML = 0
+    head_tot =0
+    //if (tail_tot > 0)
+    document.getElementById("tails").innerHTML= 0
+    document.getElementById("tails-percent").innerHTML = 0
+    tail_tot =0 
+}
+//document.getElementById("heads-percent").innerHTML = head_percent
+//document.getElementById("tails-percent").innerHTML = tail_percent
     // TODO: Add event listener and handler for flip and clear buttons
+// Flip Button Click Handler
 
-    // Flip Button Click Handler
         // TODO: Determine flip outcome
+        
         // TODO: Update image and status message in the DOM
 
         // Update the scorboard
@@ -23,5 +55,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
+
 
 })
